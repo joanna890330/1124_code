518b660c6e903fe4b42ca9e332ffdfd3b449bbd1
joanna890330 <71689413+joanna890330@users.noreply.github.com>
2020-11-24 16:16:23 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,10 +1,9 @@
-for (let i = 0; i <= 4; i++) {
-    for (let j = 0; j <= 4; j++) {
-        led.plot(i, 2)
-        led.plot(2, i)
-    }
-    basic.pause(100)
-}
 basic.forever(function () {
-	
+    for (let i = 0; i <= 4; i++) {
+        for (let j = 0; j <= 4; j++) {
+            led.plot(i, 2)
+            led.plot(2, i)
+        }
+        basic.pause(100)
+    }
 })
